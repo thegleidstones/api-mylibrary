a2ac79f1ecfc1de798f271901dad1ef41b1054a8
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2024-04-12 23:16:26 -0300

    create methods to get a list of Publishers and create new Publishers

--- src/main/java/silvadev/com/br/mylibrary/controllers/PublisherController.java
+++ src/main/java/silvadev/com/br/mylibrary/controllers/PublisherController.java
@@ -2,12 +2,14 @@ package silvadev.com.br.mylibrary.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import silvadev.com.br.mylibrary.domain.publisher.Publisher;
+import silvadev.com.br.mylibrary.dto.publisher.PublisherIdDTO;
+import silvadev.com.br.mylibrary.dto.publisher.PublisherRequestDTO;
 import silvadev.com.br.mylibrary.services.PublisherService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/publishers")
@@ -19,5 +21,20 @@ public class PublisherController {
     public ResponseEntity<Publisher> getPublisher(@PathVariable String id) {
         Publisher publisher = this.publisherService.getPublisher(id);
         return ResponseEntity.ok(publisher);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Publisher>> getPublisher() {
+        List<Publisher> publisherList = this.publisherService.getPublisher();
+        return ResponseEntity.ok(publisherList);
+    }
+
+    @PostMapping
+    public ResponseEntity<PublisherIdDTO> createPublisher(@RequestBody PublisherRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        PublisherIdDTO publisherId = this.publisherService.createPublisher(body);
+
+        var uri = uriComponentsBuilder.path("/publishers/{id}").buildAndExpand(publisherId.publisherId()).toUri();
+
+        return ResponseEntity.created(uri).body(publisherId);
     }
 }
--- src/main/java/silvadev/com/br/mylibrary/services/PublisherService.java
+++ src/main/java/silvadev/com/br/mylibrary/services/PublisherService.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Service;
 import silvadev.com.br.mylibrary.domain.publisher.Publisher;
 import silvadev.com.br.mylibrary.domain.publisher.exceptions.PublisherNotFoundException;
 import silvadev.com.br.mylibrary.dto.publisher.PublisherIdDTO;
+import silvadev.com.br.mylibrary.dto.publisher.PublisherRequestDTO;
 import silvadev.com.br.mylibrary.repositories.PublisherRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +18,19 @@ public class PublisherService {
 
     public Publisher getPublisher(String publisherId) {
         return this.publisherRepository.findById(publisherId).orElseThrow(() -> new PublisherNotFoundException("Publisher not found with ID: " + publisherId));
+    }
+
+    public List<Publisher> getPublisher() {
+        return this.publisherRepository.findAll();
+    }
+
+    public PublisherIdDTO createPublisher(PublisherRequestDTO publisherDTO) {
+        Publisher newPublisher = new Publisher();
+        newPublisher.setName(publisherDTO.name());
+        newPublisher.setCreatedAt(LocalDateTime.now());
+
+        this.publisherRepository.save(newPublisher);
+
+        return new PublisherIdDTO(newPublisher.getId());
     }
 }
