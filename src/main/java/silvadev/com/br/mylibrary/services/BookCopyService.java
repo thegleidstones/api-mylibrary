b25563cc9790f88bf0d6eba680ddc85a871e097d
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2024-04-16 22:35:47 -0300

    Create BookCopy Service

---
+++ src/main/java/silvadev/com/br/mylibrary/services/BookCopyService.java
@@ -0,0 +1,42 @@
+package silvadev.com.br.mylibrary.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import silvadev.com.br.mylibrary.domain.bookCopy.BookCopy;
+import silvadev.com.br.mylibrary.domain.bookCopy.exceptions.BookCopyNotFoundException;
+import silvadev.com.br.mylibrary.dto.bookCopy.BookCopyIdDTO;
+import silvadev.com.br.mylibrary.dto.bookCopy.BookCopyRequestDTO;
+import silvadev.com.br.mylibrary.repositories.BookCopyRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookCopyService {
+    private final BookCopyRepository bookCopyRepository;
+
+    public BookCopy getBookCopy(String id) {
+        return this.bookCopyRepository.findById(id).orElseThrow(
+                () -> new BookCopyNotFoundException("Book copy not found with ID: " + id));
+    }
+
+    public List<BookCopy> getBookCopy() {
+        return this.bookCopyRepository.findAll();
+    }
+
+    public BookCopyIdDTO createBookCopy(BookCopyRequestDTO dto) {
+        BookCopy bookCopy = new BookCopy();
+        bookCopy.setBook(dto.book());
+        bookCopy.setPublisher(dto.publisher());
+        bookCopy.setIsbnCode(dto.isbnCode());
+        bookCopy.setEdition(dto.edition());
+        bookCopy.setEditionDate(dto.editionDate());
+        bookCopy.setAcquisitionAt(dto.acquisitionAt());
+        bookCopy.setCreatedAt(LocalDateTime.now());
+
+        this.bookCopyRepository.save(bookCopy);
+
+        return new BookCopyIdDTO(bookCopy.getId());
+    }
+}
