f70e91508a2bef29b441af4a27639db7a255fdcf
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2024-05-02 21:41:57 -0300

    Book loan item Entity

---
+++ src/main/java/silvadev/com/br/mylibrary/domain/bookLoanItem/BookLoanItem.java
@@ -0,0 +1,35 @@
+package silvadev.com.br.mylibrary.domain.bookLoanItem;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import silvadev.com.br.mylibrary.domain.bookCopy.BookCopy;
+import silvadev.com.br.mylibrary.domain.bookLoan.BookLoan;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "book_loan_items")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookLoanItem {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_loan_id")
+    private BookLoan bookLoan;
+
+    @ManyToOne
+    @JoinColumn(name = "book_copy_id")
+    private BookCopy bookCopy;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
