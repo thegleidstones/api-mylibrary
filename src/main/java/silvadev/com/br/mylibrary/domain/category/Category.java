72556ca1a0672a036c0199b27b1aab0d030e9b1c
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2024-05-03 10:47:20 -0300

    Create Category Entity

---
+++ src/main/java/silvadev/com/br/mylibrary/domain/category/Category.java
@@ -0,0 +1,31 @@
+package silvadev.com.br.mylibrary.domain.category;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "is_active", nullable = true)
+    private Boolean isActive;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
