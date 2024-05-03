cbad9e0cf07afe117a13d2b31eee9f21be3242f9
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2024-05-02 21:38:35 -0300

    Database tests

--- src/main/resources/db/migration/V6__create-table-bookloans.sql
+++ src/main/resources/db/migration/V6__create-table-bookloans.sql
@@ -1,9 +1,8 @@
 CREATE TABLE book_loans (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     loan_date TIMESTAMP NOT NULL,
+    devolution_date TIMESTAMP,
     book_reader_id VARCHAR(255) NOT NULL,
-    book_copy_id VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT book_loans_book_reader_id_fkey FOREIGN KEY (book_reader_id) REFERENCES book_readers(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT book_loans_book_copy_id_fkey FOREIGN KEY (book_copy_id) REFERENCES book_copies(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT book_loans_book_reader_id_fkey FOREIGN KEY (book_reader_id) REFERENCES book_readers(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
